2a371d11909af87970850fd72b968c4a7ea77992
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-08-05 11:58:35 -0500

    git pull

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+// const pport = require('passport');
+// const pportLocal = require('passport-local');
+ const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
