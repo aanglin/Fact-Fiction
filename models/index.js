6e0010007cd99f0802f6e32394854072425de75e
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-04 19:35:10 -0500

    Added Seed data for laws.json then added model User and Laws

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const Laws = require("./Laws");
+const FakeNews = require("./fakenews");
+const User = require("./User");
+const Answers = require("./answers")
+
+
+
+module.exports = { Laws, FakeNews, User, Answers };
