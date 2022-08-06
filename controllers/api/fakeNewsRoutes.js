3cf969213e059c98e4010a8a992b602bb9a4d8d4
Sara Baqla <missatrox44@gmail.com>
2022-08-06 11:51:53 -0500

    started public js file

--- controllers/api/fakeNewsRoutes.js
+++ controllers/api/fakeNewsRoutes.js
@@ -1,20 +1,6 @@
 const router = require('express').Router();
 const { FakeNews } = require('../../models');
 
-// This route uses async/await with '.catch()' for errors
-// and no HTTP status codes
-// router.get('/', async (req, res) => {
-//   const fakeNewsData = await FakeNews.findAll()
-
-//.catch((err) => {
-//     res.json(err);
-//   });
-//   console.log(fakeNewsData)
-  // const fakeNews = fakeNewsData.map((fakeNews) => FakeNews.get({ plain: true }));
-  //     res.render('all', { fakeNews });
-// });
-
-
 //Promise route IT WORKS!!!
 //test get route localhost3001/fakenews
 router.get('/', (req,res) => {
