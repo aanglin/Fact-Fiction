2753b2b3f476c6cb7f120217e98849c3c5dc603e
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-08-08 11:12:33 -0500

    adding front end logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,23 @@
 const loginFormHandler = async function(event) {
     event.preventDefault();
   
-    const usernameEl = document.querySelector("#username-input-login");
-    const passwordEl = document.querySelector("#password-input-login");
-    fetch("/api/user/login", {
+    const usernameEl = document.querySelector("#username");
+    const passwordEl = document.querySelector("#password");
+    fetch("/api/userRoutes", {
       method: "post",
       body: JSON.stringify({
-        username: usernameEl.value,
+        userName: usernameEl.value,
         password: passwordEl.value
       }),
       headers: { "Content-Type": "application/json" }
     })
       .then(function() {
-        document.location.replace("/dashboard");
+        document.location.replace("/gamePage");
       })
       .catch(err => console.log(err));
   };
   
   document
-    .querySelector("#login-form")
+    .querySelector("#login")
     .addEventListener("submit", loginFormHandler);
   
