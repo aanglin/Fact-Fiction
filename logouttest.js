23b1c989a32a39bf470968d1b125512ddbc26ec8
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-08-08 20:32:47 -0500

    fixed test package

--- logouttest.js
+++ logouttest.js
@@ -3,14 +3,17 @@ const nightmare = Nightmare({ show: true })
 
 nightmare
   .goto('http://localhost:3001/gamepage')
-  .click('#nightmare')
+   .click('#nightmare')
   .click('#logout')
-//   .type('#users', 'aanglin')
-//   .type('#password', 'password123')
-//   .click('#login')
   .wait(3000)
   .end()
-  .then(nightmare.goto('http://google.com'))
+  .then(nightmare
+    .goto("http://localhost:3001")
+    .type("#username-input-signup", "Jaredplus")
+    .type("#password-input-signup", "password123")
+    .click("#signup-form")
+    .wait(3000)
+    .end())
   .catch(error => {
     console.error('Search failed:', error)
   })
